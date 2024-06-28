b7bfcc88ca933849efa6591806c0bb051fd749e3
jimmyrai321 <jimmyrai321@gmail.com>
2024-06-28 16:23:58 +0100

    feat: created MVVM structure, need to create Journey model

---
+++ app/src/main/java/com/example/tracktraveldisruptionsapp/model/Journey.java
@@ -0,0 +1,4 @@
+package com.example.tracktraveldisruptionsapp.model;
+
+public class Journey {
+}
---
+++ app/src/main/java/com/example/tracktraveldisruptionsapp/model/JourneyRepository.java
@@ -0,0 +1,44 @@
+package com.example.tracktraveldisruptionsapp.model;
+
+import android.app.Application;
+import android.util.Log;
+import androidx.lifecycle.MutableLiveData;
+import com.example.tracktraveldisruptionsapp.service.RetrofitInstance;
+import com.example.tracktraveldisruptionsapp.service.TTDApiService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.List;
+
+public class JourneyRepository {
+    private MutableLiveData<List<Journey>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public JourneyRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Journey>> getMutableLiveData() {
+        TTDApiService service = RetrofitInstance.getService();
+        Call<List<Journey>> call = service.getAllJourneys();
+        call.enqueue(new Callback<List<Journey>>() {
+            @Override
+            public void onResponse(Call<List<Journey>> call, Response<List<Journey>> response) {
+                List<Journey> journeys = response.body();
+                Log.i("JOURNEYLISTLOG",""+response.code());
+                Log.i("JOURNEYLISTLOG", journeys.toString());
+                Log.i("JOURNEYLISTLOG","ON SUCCESS");
+
+
+                mutableLiveData.setValue(journeys);
+            }
+
+            @Override
+            public void onFailure(Call<List<Journey>> call, Throwable throwable) {
+
+            }
+        });
+        return mutableLiveData;
+    }
+}
