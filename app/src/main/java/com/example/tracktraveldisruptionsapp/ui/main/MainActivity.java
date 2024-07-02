acd96e267f4bd0b0028ef6467a011c64abea28d7
jimmyrai321 <jimmyrai321@gmail.com>
2024-07-02 17:13:47 +0100

    feat: Time picker implemented

--- app/src/main/java/com/example/tracktraveldisruptionsapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/tracktraveldisruptionsapp/ui/main/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.tracktraveldisruptionsapp.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.tracktraveldisruptionsapp.NewJourneyActivity;
+import com.example.tracktraveldisruptionsapp.ui.addjourney.NewJourneyActivity;
 import com.example.tracktraveldisruptionsapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
